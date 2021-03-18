cb279897eedb7930027102980e6a00b62e30a331
the company <thecompany001@protonmail.com>
2021-03-17 23:32:10 -0400

    Snake movement

--- snake.py
+++ snake.py
@@ -14,11 +14,32 @@ class cube(object):
         pass
 
 class snake(object):
+    body = []
+    turns = {}
     def __init__(self, color, pos):
-        pass
+        self.color = color
+        self.head = cube(pos)
+        self.body.append(self.head)
+        self.dirnx = 0
+        self.diry = 1
 
     def move(self):
-        pass
+        for event in pygame.event.get():
+            if event.type === pygame.QUIT:
+                pygame.quit()
+
+                keys = pygame.key.get_pressed()
+
+                for key in keys: 
+                    if keys[pygame.K_LEFT]:
+                        self.dirnx = -1
+                        self.dirny = 0
+                        self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]]
+                    if keys[pygame.K_RIGHT]:
+                    if keys[pygame.K_UP]:
+                    if keys[pygame.K_DOWN]:
+
+                    
     
     def reset(self, pos):
         pass

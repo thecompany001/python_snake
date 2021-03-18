72f0dde3c9393e95df2a559e70da6b916b06be00
the company <thecompany001@protonmail.com>
2021-03-17 23:44:59 -0400

    Snake tail movement

--- snake.py
+++ snake.py
@@ -35,11 +35,35 @@ class snake(object):
                         self.dirnx = -1
                         self.dirny = 0
                         self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]]
-                    if keys[pygame.K_RIGHT]:
-                    if keys[pygame.K_UP]:
-                    if keys[pygame.K_DOWN]:
 
-                    
+                    elif keys[pygame.K_RIGHT]:
+                        self.dirnx = 1
+                        self.dirny = 0
+                        self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]]
+
+                    elif keys[pygame.K_UP]:
+                        self.dirnx = 0
+                        self.dirny = -1
+                        self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]]
+
+                    elif keys[pygame.K_DOWN]:
+                        self.dirnx = 0
+                        self.dirny = 1
+                        self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]]
+
+        for i, c in enumerate(self.body):
+            p = c.pos[:]
+            if p is in self.turns:
+                turn = self.turns[p]
+                c.move(turn[0], turn[1])
+                if i == len(self.body)-1:
+                    self.turns.pop(p)
+            else:
+                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
+                elif c.dirnx == 1 and c.pos[0] >= 0: c.rows-1: c.pos = (0,c.pos[1])
+                elif c.dirny == 1 and c.pos[1] >= 0: c.rows-1: c.pos = (c.pos[0], 0)
+                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0], c.rows-1)
+
     
     def reset(self, pos):
         pass
